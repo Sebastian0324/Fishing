3493109da5e4fdc63e96d3dd9858057559a79396
Sebastian <sebbeelverhoj@gmail.com>
2025-10-07 09:03:37 +0200

    Cleand up front end
    Cleand up the front end and added a form to sign in and to sign up, added some JS and CSS in order to get it to function and look somewhat good

--- Fish/static/script.js
+++ Fish/static/script.js
@@ -1,19 +1,31 @@
-document.getElementById("uploadForm").onsubmit = async (e) => {
-  e.preventDefault();
+let login = document.getElementById("Login");
 
-  let formData = new FormData(e.target);
-  let response = await fetch("/upload", { method: "POST", body: formData });
-  let data = await response.json();
-  
-  document.getElementById("result").innerHTML =
-    `<b>${data.subject}</b><br>From: ${data.from}<br>${data.preview}`;
-};
-
-let form = document.getElementById("Login");
 document.getElementById("Login_btn").addEventListener("click", function() {
-  form.style.display = "block";
-  console.log("Block");
+  login.style.display = "block";
 });
 document.getElementById("Close_Login").addEventListener("click", function() {
-  form.style.display = "None";
+  document.getElementById("Login").style.display = "None";
 });
+
+let SignIn = document.getElementById("SignIn");
+let SignUp = document.getElementById("SignUp");
+document.getElementById("SignIn/Up").addEventListener("click", function() {
+    SignIn.classList.toggle("hidden");
+    SignUp.classList.toggle("hidden");
+});
+
+
+// -------========-------    Front Page    -------========-------
+let UpForm = document.getElementById("uploadForm");
+if (UpForm != null) {
+  UpForm.onsubmit = async (e) => {
+    e.preventDefault();
+
+    let formData = new FormData(e.target);
+    let response = await fetch("/upload", { method: "POST", body: formData });
+    let data = await response.json();
+    
+    document.getElementById("result").innerHTML =
+      `<b>${data.subject}</b><br>From: ${data.from}<br>${data.preview}`;
+  };
+}
